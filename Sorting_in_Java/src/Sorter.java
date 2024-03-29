6cda9669787213648c20d1786a844dbb05dfb9e3
CodexPremiera <ashleyken.professional@gmail.com>
2024-03-30 02:28:42 +0800

    implement selection sort in java

--- Sorting_in_Java/src/Sorter.java
+++ Sorting_in_Java/src/Sorter.java
@@ -8,9 +8,8 @@ public class Sorter {
      * BUBBLE SORT
      *
      * <p> The bubble sort algorithm is infamous for being so slow.
-     * The algorithm iterates through the list. On each iteration,
-     * it iterates again to find the largest element of the list
-     * and 'bubble-it-up' to the end.
+     * The algorithm iterates through the list, finds the largest
+     * element in each iteration and 'bubbles-it-up' to the end.
      *
      * <li> Worst-case Time Complexity: `O(n²)`
      * <li> Average-case Time Complexity: `O(n²)`
@@ -45,4 +44,43 @@ public class Sorter {
         }
     } // END OF BUBBLE SORT
 
+
+    /**
+     * SELECTION SORT
+     *
+     * <p> The selection sort algorithm divides the list into two sections:
+     * the sorted section and the unsorted section. The algorithm iterates
+     * through the list, 'selects' the index of the smallest in each iteration
+     * and places it to the current position.
+     *
+     * <li> Worst-case Time Complexity: `O(n²)`
+     * <li> Average-case Time Complexity: `O(n²)`
+     * <li> Best-case Time Complexity: `O(n²)`
+     * */
+    public static <T extends Comparable<T>> void selection(List<T> list) {
+        int size = list.size();
+        int last = size - 1;
+
+        for (int i = 0; i < last; i++) {
+            int minIndex = i;
+
+
+            // iterate only through the unsorted section
+            for (int j = i; j < size; j++) {
+                T current = list.get(j);
+                T minElement = list.get(minIndex);
+
+                boolean currentIsLesser = current.compareTo(minElement) < 0;
+                if (currentIsLesser)
+                    minIndex = j;
+            }
+
+            // swap with min index
+            if (minIndex != i) {
+                T temp = list.get(i);
+                list.set(i, list.get(minIndex));
+                list.set(minIndex, temp);
+            }
+        }
+    } // END OF SELECTION SORT
 }
