c0ee078c40bb740fa705da51c981213651f9469b
CodexPremiera <ashleyken.professional@gmail.com>
2024-03-30 03:23:25 +0800

    make shell sort tester in java

--- Sorting_in_Java/test/SorterTest.java
+++ Sorting_in_Java/test/SorterTest.java
@@ -9,6 +9,39 @@ import java.util.List;
 public class SorterTest {
     private final int sampleSize = 8000;
 
+    /* HELPER METHODS */
+    public <T extends Comparable<T>> List<T> generateList(int size, Class<T> type) {
+        List<T> list = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            list.add(generateRandomValue(type));
+        }
+        return list;
+    }
+
+    private <T extends Comparable<T>> T generateRandomValue(Class<T> type) {
+        if (type == Integer.class)
+            return type.cast((int) (Math.random() * 2000));
+
+        else if (type == Double.class)
+            return type.cast(Math.random() * 2000);
+
+        else if (type == Character.class)
+            return type.cast((char) ('a' + Math.random() * 26));
+
+        else if (type == String.class) {
+            StringBuilder sb = new StringBuilder();
+            int length = (int) (Math.random() * 10) + 1;
+            for (int i = 0; i < length; i++) {
+                sb.append((char) ('a' + Math.random() * 26));
+            }
+            return type.cast(sb.toString());
+        }
+
+        else
+            throw new IllegalArgumentException("Unsupported type: " + type.getSimpleName());
+    }
+
+
     /* TEST BUBBLE SORT */
     @Test
     public void testBubbleSort() {
@@ -102,39 +135,36 @@ public class SorterTest {
         Sorter.insertion(input);
         Assertions.assertEquals(expected, input);
     } // END TEST INSERTION SORT
-    
-    
 
-    /* HELPER METHODS */
-    public <T extends Comparable<T>> List<T> generateList(int size, Class<T> type) {
-        List<T> list = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            list.add(generateRandomValue(type));
-        }
-        return list;
+
+    
+    /* TEST SHELL SORT */
+    @Test
+    public void testShellSort() {
+        testShellSortByClass(Integer.class);
+        testShellSortByClass(Double.class);
+        testShellSortByClass(Character.class);
+        testShellSortByClass(String.class);
     }
 
-    private <T extends Comparable<T>> T generateRandomValue(Class<T> type) {
-        if (type == Integer.class)
-            return type.cast((int) (Math.random() * 1000));
+    private <T extends Comparable<T>> void testShellSortByClass(Class<T> type) {
+        // make lists of certain size
+        List<T> list = generateList(sampleSize, type);
 
-        else if (type == Double.class)
-            return type.cast(Math.random() * 1000);
+        List<T> sortedList = new ArrayList<>(list);
+        Collections.sort(sortedList);
 
-        else if (type == Character.class)
-            return type.cast((char) ('a' + Math.random() * 26));
+        List<T> reverseList = new ArrayList<>(sortedList);
+        Collections.reverse(reverseList);
 
-        else if (type == String.class) {
-            StringBuilder sb = new StringBuilder();
-            int length = (int) (Math.random() * 10) + 1;
-            for (int i = 0; i < length; i++) {
-                sb.append((char) ('a' + Math.random() * 26));
-            }
-            return type.cast(sb.toString());
-        }
-
-        else
-            throw new IllegalArgumentException("Unsupported type: " + type.getSimpleName());
+        // test lists
+        testShellSortAList(list, sortedList);
+        testShellSortAList(sortedList, sortedList);
+        testShellSortAList(reverseList, sortedList);
     }
 
+    <T extends Comparable<T>> void testShellSortAList(List<T> input, List<T> expected) {
+        Sorter.shell(input);
+        Assertions.assertEquals(expected, input);
+    } // END TEST SHELL SORT
 }
